bb73ec7600d355d4e744df072165b33e03e94565
Cyrille37 <Cyrille37@gmail.com>
2013-07-20 02:30:10 +0200

    continuing initial coding

--- demo/demo.js
+++ demo/demo.js
@@ -1,44 +1,27 @@
+
 jQuery(document).ready( function($) {
 
 	var form ;
 
 	// Hide email auth forms
 	$('#auth-form-email').hide();
+	$('#auth-form-email .auth-email-error').hide();
 	$('#auth-form-email-code').hide();
 
 	// Define authentification services icons onclick()
 	form = $('#auth-form');
 	$('.auth-email-button', form).click(function() {
-		auth_email();
+		Yasnal.authEmail();
 	});
 
 	// Define email authentification service onclick()
 	form = $('#auth-form-email');
 
 	$('.auth-email-send', form).click(function() {
-		auth_email('emailSend');
+		Yasnal.authEmail('emailSend');
 	});
 	$('.auth-email-sendCancel', form).click(function() {
-		auth_email('emailSendCancel');
+		Yasnal.authEmail('emailSendCancel');
 	});
 
 });
-
-function auth_email(step) {
-
-	var form = jQuery('#auth-form');
-
-	switch(step){
-	case undefined :
-		jQuery('#auth-form').hide();
-		jQuery('#auth-form-email').show();
-		break;
-	case 'emailSendCancel':
-		jQuery('#auth-form-email').hide();
-		jQuery('#auth-form').show();
-		break;
-	case 'emailSend':
-		break;
-	}
-
-}
--- demo/index.php
+++ demo/index.php
@@ -14,6 +14,10 @@ define( 'YASNAL_URI',  dirname($_SERVER['PHP_SELF']).'/../lib' );
 
 		<link rel="stylesheet" type="text/css" media="all" href="demo.css" />
 		<script src="jquery.min.js" ></script>
+		<script src="<?php echo YASNAL_URI,'/Yasnal.js' ?>" ></script>
+		<script>
+			Yasnal.config.lib_uri = '<?php echo YASNAL_URI ?>' ;
+		</script>
 		<script src="demo.js" ></script>
    </head>
     <body>
@@ -29,16 +33,16 @@ define( 'YASNAL_URI',  dirname($_SERVER['PHP_SELF']).'/../lib' );
 					<li><a href="javascript:void(0);" title="identifiez vous avec Google+" class="auth-google-button"><img alt="Google" src="<?php echo YASNAL_URI,'/img/google_32.png' ?>" /></a></li>
 					<li><a href="javascript:void(0);" title="identifiez vous avec un email" class="auth-email-button"><img alt="eMail" src="<?php echo YASNAL_URI,'/img/email_32.jpg' ?>" /></a></li>
 				</ul>
-				<input type="hidden" class="auth-email-redirectUri" name="redirect_uri" value="<?php echo YASNAL_URI,'/email/connect.php' ?>" />
 			</form>
 		</div>
 		<div id="auth-form-email">
 			<form title="Email Authentification">
 				<p>Merci d'indiquer votre adresse email à laquelle nous allons vous expédier un code pour vous identifier:</p>
 				<p>
-					Votre adresse email: <input type="text" name="email" size="30" />
-					<input type="button" value="Envoyer le code" class="auth-email-send"  onclick="return auth_email('emailSend');" />
-					<input type="button" value="Annuler" class="auth-email-sendCancel"  onclick="return auth_email('emailSendCancel');" />
+					Votre adresse email: <input type="text" name="auth-email" size="30" />
+					<div class="auth-email-error">error message goes here</div>
+					<input type="button" value="Envoyer le code" class="auth-email-send" />
+					<input type="button" value="Annuler" class="auth-email-sendCancel" />
 				</p>
 			</form>
 		</div>
@@ -46,9 +50,9 @@ define( 'YASNAL_URI',  dirname($_SERVER['PHP_SELF']).'/../lib' );
 			<form title="Social Authentification">
 				<p>Indiquer le code que vous avez reçu à l'adresse email <span id="auth-email"></span></p>
 				<p>
-					Le code: <input type="text" name="emailCode" size="10" />
-					<input type="button" value="Valider" onclick="return auth_email('emailCode');" />
-					<input type="button" value="Annuler" onclick="return auth_email('emailCodeCancel');" />
+					Le code: <input type="text" name="auth-emailCode" size="10" />
+					<input type="button" value="Valider" />
+					<input type="button" value="Annuler" />
 				</p>
 			</form>
 		</div>
